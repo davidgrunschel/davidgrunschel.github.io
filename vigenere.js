ce1ecff5c9de41eb9c43386d627fe14a2ad1ff8f
davidgrunschel <71101323+davidgrunschel@users.noreply.github.com>
2020-09-20 20:47:13 -0600

    Add a couple of spaces.

--- vigenere.js
+++ vigenere.js
@@ -9,8 +9,8 @@ function toCode() {
     for (i = 0; i < theMess.length; i++) {
         if (j == theKey.length)  {j = 0}
         iMess = theMess.charCodeAt(i);
-        if (iMess == 10)  {iMess = 32}  // If a line feed is entered, change it to a space.
-        if (iMess == 96)  {iMess = 39}  // If a ` character is entered, change it to a '.
+        if (iMess == 10)  {iMess = 32}   // If a line feed is entered, change it to a space.
+        if (iMess == 96)  {iMess = 39}   // If a ` character is entered, change it to a '.
         if (iMess == 123)  {iMess = 40}  // If a { character is entered, change it to a (.
         if (iMess == 124)  {iMess = 47}  // If a | character is entered, change it to a /.
         if (iMess == 125)  {iMess = 41}  // If a } character is entered, change it to a ).
